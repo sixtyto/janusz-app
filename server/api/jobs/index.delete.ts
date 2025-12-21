e70ed10f02f7d3f7fc39c968c4922a3ddf440c1c
Piotr Czajkowski <sixtyto@gmail.com>
2025-12-21 09:39:51 +0100

    refactor(jobs): remove redundant type checking

--- server/api/jobs/index.delete.ts
+++ server/api/jobs/index.delete.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
-  const id = typeof query.id === 'string' ? query.id.toString() : undefined
+  const id = typeof query.id === 'string' ? query.id : undefined
 
   if (!id) {
     throw createError({ status: 400, message: 'Missing job ID' })
