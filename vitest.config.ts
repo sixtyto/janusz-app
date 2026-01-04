a80f4fcf2868e25a0ab4a3a5d31d3b32440d1bf4
Piotr Czajkowski <sixtyto@gmail.com>
2026-01-04 17:04:24 +0100

    feature: add coverage support using V8 in Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,26 @@
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
+
+const currentDirectory = dirname(fileURLToPath(import.meta.url))
 
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
+    root: currentDirectory,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text'],
+      include: ['server/**/*.ts', 'app/**/*.ts'],
+      exclude: [
+        'shared/types/**',
+        'nuxt.config.ts',
+        'server/utils/createLogger.ts',
+        'server/utils/getRedisClient.ts',
+        'server/utils/getPrReviewQueue.ts',
+        'server/utils/startWorker.ts',
+        'server/plugins/worker.ts',
+      ],
+    },
   },
 })
