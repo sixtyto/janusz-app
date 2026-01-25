d150da92b7c3a7a5177f7a7f51c2f0b84ddefd55
Piotr Czajkowski <sixtyto@gmail.com>
2026-01-25 23:56:26 +0100

    feat(queue): integrate Bull Board (#85)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -65,15 +65,17 @@ export default defineNuxtConfig({
     },
   },
   nitro: {
-    moduleSideEffects: [
-      '@bull-board/ui/package.json',
-      '@bull-board/api',
-      '@bull-board/ui',
-    ],
-  },
-  vite: {
-    optimizeDeps: {
-      include: ['@bull-board/ui', '@bull-board/api'],
+    hooks: {
+      compiled: async (nitro) => {
+        const { cp } = await import('node:fs/promises')
+        const { resolve, join } = await import('node:path')
+
+        const sourceUIDirectory = resolve('./node_modules/@bull-board/ui')
+        const targetUIDirectory = join(nitro.options.output.serverDir, 'node_modules/@bull-board/ui')
+
+        await cp(sourceUIDirectory, targetUIDirectory, { recursive: true })
+        console.log('[nitro] Copied @bull-board/ui to output')
+      },
     },
   },
   runtimeConfig: {
